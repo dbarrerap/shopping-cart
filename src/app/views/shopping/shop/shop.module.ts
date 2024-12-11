9fe94085780f35e6b9b72299de40f23d1c22ffc9
David E. Barrera <dbarrerap@live.com>
2024-12-11 14:44:07 -0500

    Limpieza

--- src/app/views/shopping/shop/shop.module.ts
+++ src/app/views/shopping/shop/shop.module.ts
@@ -3,12 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ShopRoutingModule } from './shop-routing.module';
 import { ShopComponent } from "./shop.component";
-import { ShoppingItemComponent } from './shopping-item/shopping-item.component'
 
 import { AppCustomModule } from "src/app/shared/app-custom.module";
 import { WalletComponent } from './wallet/wallet.component';
 import { SearchComponent } from './search/search.component';
-import { SarchItemComponent } from "./search/sarch-item/sarch-item.component";
 
 
 
@@ -16,10 +14,8 @@ import { SarchItemComponent } from "./search/sarch-item/sarch-item.component";
 @NgModule({
   declarations: [
     ShopComponent, 
-    ShoppingItemComponent, 
     WalletComponent, 
     SearchComponent,
-    SarchItemComponent,
   ],
   imports: [
     CommonModule,
