06d7452525146dcbca168a7f01e964ef92175a9c
David E. Barrera <dbarrerap@live.com>
2024-12-11 14:43:52 -0500

    Implementacion Vertical Item

--- src/app/shared/components/vertical-item/vertical-item.component.ts
+++ src/app/shared/components/vertical-item/vertical-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Producto } from '../../models';
 
 @Component({
   selector: 'app-vertical-item',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './vertical-item.component.scss'
 })
 export class VerticalItemComponent {
+  @Input() producto!: Producto
+  public cantidad: number = 0
 
+  increment = (valor: number) => {
+    this.cantidad += valor;
+    if (this.cantidad > parseInt(this.producto.stock as string)) this.cantidad = parseInt(this.producto.stock as string)
+  }
+
+  decrement = (valor: number) => {
+    this.cantidad -= valor
+    if (this.cantidad < 0) this.cantidad = 0
+  }
 }
