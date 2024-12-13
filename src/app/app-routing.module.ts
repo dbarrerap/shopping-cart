61ad1d2a442f072c5408178744e193f839751ff7
David E. Barrera <dbarrerap@live.com>
2024-12-13 16:50:29 -0500

    Build success

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { DefaultLayoutComponent, ShoppingLayoutComponent } from './containers';
+import { 
+  DefaultLayoutComponent, 
+  ShoppingLayoutComponent 
+} from './containers';
 import { Page404Component } from './views/pages/page404/page404.component';
 import { Page500Component } from './views/pages/page500/page500.component';
 import { LoginComponent } from './views/pages/login/login.component';
@@ -85,35 +88,35 @@ const routes: Routes = [
       }
     ]
   },
-  // {
-  //   path: '404',
-  //   component: Page404Component,
-  //   data: {
-  //     title: 'Page 404'
-  //   }
-  // },
-  // {
-  //   path: '500',
-  //   component: Page500Component,
-  //   data: {
-  //     title: 'Page 500'
-  //   }
-  // },
-  // {
-  //   path: 'login',
-  //   component: LoginComponent,
-  //   data: {
-  //     title: 'Login Page'
-  //   }
-  // },
-  // {
-  //   path: 'register',
-  //   component: RegisterComponent,
-  //   data: {
-  //     title: 'Register Page'
-  //   }
-  // },
-  {path: '**', redirectTo: 'dashboard'}
+  {
+    path: '404',
+    component: Page404Component,
+    data: {
+      title: 'Page 404'
+    }
+  },
+  {
+    path: '500',
+    component: Page500Component,
+    data: {
+      title: 'Page 500'
+    }
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: {
+      title: 'Login Page'
+    }
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    data: {
+      title: 'Register Page'
+    }
+  },
+  {path: '**', redirectTo: 'shopping'}
 ];
 
 @NgModule({
