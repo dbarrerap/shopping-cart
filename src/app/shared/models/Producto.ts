9931dbdac89f63a3ad8cd6dcabc49a8ac6561f66
David E. Barrera <dbarrerap@live.com>
2024-12-23 09:35:38 -0500

    Agregar campos

--- src/app/shared/models/Producto.ts
+++ src/app/shared/models/Producto.ts
@@ -3,17 +3,27 @@ export interface Producto {
     id_producto?: number
     codigo: string
     nombre: string
+    nombrecorto?: string
+    marca?: string
     descripcion: string
     precio: number | string
     oferta?: boolean
     descuento?: number|string
     precio_oferta?: number|string
     stock?: number | string
-    imagen: string
+    imagen?: string
     cantidad?: number|string
+    estado?: string
+    subtotal?: number
+    porcentaje?: number
+    total?: number
+    iva?:number
+    total_final?: number
     fotos?: ProductoFoto[]
 }
 
 interface ProductoFoto {
+    id_producto_foto?: number
+    fk_producto?: number
     recurso: string
 }
