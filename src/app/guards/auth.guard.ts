972cc378204c8bb7f22d19a1541eb7740a4b141a
David E. Barrera <dbarrerap@live.com>
2024-12-30 21:15:50 -0500

    Simplificacion de Guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,18 +2,17 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from "../services/auth.service";
 import { inject } from '@angular/core';
 
+const LOGIN_ROUTE: string = '/login'
+const DEFAULT_ROUTE: string = '/shopping'
+
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService)
-  if (authService.isLoggedIn()) return true;
 
-  // return authService.isLoggedIn()
-  return inject(Router).createUrlTree(['/login']);
+  return authService.isLoggedIn() || inject(Router).createUrlTree([LOGIN_ROUTE])
 };
 
 export const loginGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService)
-  if (!authService.isLoggedIn()) return true;
   
-  // return !authService.isLoggedIn()
-  return inject(Router).createUrlTree(['/shopping']);
+  return !authService.isLoggedIn() || inject(Router).createUrlTree([DEFAULT_ROUTE])
 };
